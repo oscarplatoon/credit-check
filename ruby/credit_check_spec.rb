fcf78369b443003c3b70481f9b4096f2e3de8661
Jonathan Young <jonyoungg@gmail.com>
2017-01-25 13:11:17 -0600

    credit check

---
+++ ruby/credit_check_spec.rb
@@ -0,0 +1,9 @@
+require_relative 'check_credit'
+
+puts check_credit('5541808923795240') == "The number is valid!"
+puts check_credit("4024007136512380") == "The number is valid!"
+puts check_credit("6011797668867828") == "The number is valid!"
+
+puts check_credit("5541801923795240") == "The number is invalid!"
+puts check_credit("4024007106512380") == "The number is invalid!"
+puts check_credit("6011797668868728") == "The number is invalid!"
