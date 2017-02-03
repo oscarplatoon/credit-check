376d6097c632163a15bdeeb1c9cb5c9c693a25f0
Jonathan Young <jonyoungg@gmail.com>
2017-02-03 14:48:05 -0600

    Update credit_check.rb

--- ruby/credit_check.rb
+++ ruby/credit_check.rb
@@ -1,9 +1,3 @@
-# Start with this template
-
-card_number = "4929735477250543"
-
-valid = false
-
 def credit_check
 
 end
@@ -12,9 +6,3 @@ end
 # Expected Output:
 # If it is valid, print "The number is valid!"
 # If it is invalid, print "The number is invalid!"
-
-# Hints
-
-# You don't need any kind of loops, iteration, or arrays (You can certainly use them, but they aren't required)
-# You can pull a character out of a string using `my_string[X]` where `X` is a numeric position number
-# You can convert a string to an integer by calling `.to_i` (ex: `"4".to_i`)
