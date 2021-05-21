3ebcd0420f4e6400a8e6dc4c956ef04e1825e960
Won Yang <won@gatech.edu>
2021-05-21 12:01:55 -0500

    adds python solution

--- python/credit_check_spec.py
+++ python/credit_check_spec.py
@@ -1,7 +1,29 @@
-# Can you translate this driver code to unit tests?
-
+import unittest
 from credit_check import credit_check
 
+class TestCreditCheck(unittest.TestCase):
+    def test_returns_string(self):
+        self.assertEqual(type(credit_check("123")) == str, True)
+
+    def test_works_for_case_1(self):
+        self.assertEqual(credit_check("5541808923795240"), "The number is valid!")
+
+    def test_works_for_case_2(self):
+        self.assertEqual(credit_check("4024007136512380"), "The number is valid!")
+    
+    def test_works_for_case_3(self):
+        self.assertEqual(credit_check("6011797668867828"), "The number is valid!") 
+    
+    def test_works_for_case_4(self):
+        self.assertEqual(credit_check("5541801923795240"), "The number is invalid!")
+    
+    def test_works_for_case_5(self):
+        self.assertEqual(credit_check("4024007106512380"), "The number is invalid!")
+
+    def test_works_for_case_6(self):
+        self.assertEqual(credit_check("6011797668868728"), "The number is invalid!") 
+
+"""
 print(credit_check('5541808923795240') == "The number is valid!")
 print(credit_check("4024007136512380") == "The number is valid!")
 print(credit_check("6011797668867828") == "The number is valid!")
@@ -9,3 +31,4 @@ print(credit_check("6011797668867828") == "The number is valid!")
 print(credit_check("5541801923795240") == "The number is invalid!")
 print(credit_check("4024007106512380") == "The number is invalid!")
 print(credit_check("6011797668868728") == "The number is invalid!")
+"""
