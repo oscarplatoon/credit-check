df270fd24450157abf4668f956a2fbcfefba4aea
stesun2 <ss27@illinois.edu>
2021-05-27 16:43:05 -0500

    credit_check.py portion completed

--- python/credit_check.py
+++ python/credit_check.py
@@ -1,8 +1,46 @@
+# import numpy as np
+
 def credit_check(str):
-    pass
+
+    # Check credit length: 16
+
+    str_arr = []
+
+    # Seperating into single string
+    for i in (str):
+        # print(i)
+        str_arr.append(i)
+
+    # Converting string into int
+    int_list = list(map(int, str_arr))
+    # print(int_list)
+
+    # Right to left double ever other digit
+    # If digit > 9 return digit - 9
+    new_list = []
+    for i in range(0, len(int_list)):
+        if (i % 2 == 0):
+            new_list.append(int_list[i]*2)
+            if (new_list[i] > 9):
+                new_list[i] = (new_list[i] - 9)
+        else:
+            new_list.append(int_list[i])
+    
+    # print(new_list)
+    
+    # Sum of list
+    total_sum = sum(new_list)
+
+    if (total_sum % 10 == 0):
+        return 'The number is valid!'
+    else:
+        return 'The number is invalid!'
+
+    # pass
 
 # Your Luhn Algorithm Here
 # Expected Output:
 # If it is valid, print "The number is valid!"
 # If it is invalid, print "The number is invalid!"
 
+# print(credit_check('5541808923795240'))
--- python/credit_check_spec.py
+++ python/credit_check_spec.py
@@ -1,11 +1,31 @@
 # Can you translate this driver code to unit tests?
-
+import unittest
 from credit_check import credit_check
 
-print(credit_check('5541808923795240') == "The number is valid!")
-print(credit_check("4024007136512380") == "The number is valid!")
-print(credit_check("6011797668867828") == "The number is valid!")
+class CreditCheckTestCase(unittest.TestCase):
+    """Tests for credit_check.py"""
 
-print(credit_check("5541801923795240") == "The number is invalid!")
-print(credit_check("4024007106512380") == "The number is invalid!")
-print(credit_check("6011797668868728") == "The number is invalid!")
+    def test_return_valid_credit_check(self):
+        """When you call credit_check(), it should return valid or not"""
+        self.assertEqual(credit_check('5541808923795240'), "The number is valid!")
+        self.assertEqual(credit_check('4024007136512380'), "The number is valid!")
+        self.assertEqual(credit_check('6011797668867828'), "The number is valid!")
+    
+    def test_return_invalid_credit_check(self):
+        """When you call credit_check(), it should return valid or not"""
+        self.assertEqual(credit_check('5541801923795240'), "The number is invalid!")
+        self.assertEqual(credit_check('4024007106512380'), "The number is invalid!")
+        self.assertEqual(credit_check('6011797668868728'), "The number is invalid!")
+
+    
+
+# print(credit_check('5541808923795240') == "The number is valid!")
+# print(credit_check("4024007136512380") == "The number is valid!")
+# print(credit_check("6011797668867828") == "The number is valid!")
+
+# print(credit_check("5541801923795240") == "The number is invalid!")
+# print(credit_check("4024007106512380") == "The number is invalid!")
+# print(credit_check("6011797668868728") == "The number is invalid!")
+
+if __name__ == '__main__':
+    unittest.main()
