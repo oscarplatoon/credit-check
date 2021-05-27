96f8916997f4b49df8fe7088139ec4221b160d69
awhitford10 <awhitford@Andrews-Mac-mini.local>
2021-05-27 17:06:27 -0400

    Andrew and James_credit check_ python solution

--- python/credit_check.py
+++ python/credit_check.py
@@ -1,8 +1,22 @@
 def credit_check(str):
-    pass
+    if not str.isnumeric():
+        return('invalid input')
+    reverse_s = str[::-1]
+    sum = 0
+    for count,value in enumerate(reverse_s):
+        num = int(value)
+        if (count % 2 != 0):
+            num *= 2
+            if (num > 9):
+                num -= 9
+        sum += num
 
-# Your Luhn Algorithm Here
-# Expected Output:
-# If it is valid, print "The number is valid!"
-# If it is invalid, print "The number is invalid!"
+    
+    if(sum % 10 == 0):
+        return("The number is valid!")
+    else:
+        return("The number is invalid!")
+
+            
+
 
