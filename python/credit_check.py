55016c6fd91dbc77cfcda7f71d61bfb858cff539
RenoAverill <reno_averill@yahoo.com>
2021-05-28 22:35:10 -0600

    Reno's credit_check.py unittest and psudocode setup-ready for code next

--- python/credit_check.py
+++ python/credit_check.py
@@ -1,3 +1,23 @@
+# Account identifier:    5   5   4   1   8   0   8   9   2   3   7   9   5   2   4   0
+# 2x every other digit:  10  5   8   1   16  0   16  9   4   3   14  9   10  2   8   0
+# Summed digits over 10: 1   5   8   1   7   0   7   9   4   3   5   9   1   2   8   0
+# Results summed:        1   5   8   1   7   0   7   9   4   3   5   9   1   2   8   0 = 70
+
+# function that takes in str of numbers
+# turn string into list of individual numbers
+
+# 2x every other digit starting from right_most_digit
+
+# remove all digits greater than 10
+
+# sum up remains digits
+
+# if sum == 70 return 'The number is valid!'
+
+# if sum does not == 70 return 'The number is invalid!'
+
+
+
 def credit_check(str):
     pass
 
--- python/credit_check_spec.py
+++ python/credit_check_spec.py
@@ -1,11 +1,35 @@
 # Can you translate this driver code to unit tests?
+import unittest
+from credit_check import credit_check, is_double, sum_equal_70
 
-from credit_check import credit_check
+class TestStringMethods(unittest.TestCase):
 
-print(credit_check('5541808923795240') == "The number is valid!")
-print(credit_check("4024007136512380") == "The number is valid!")
-print(credit_check("6011797668867828") == "The number is valid!")
+    def test_double_every_other(self):
+        self.assertEqual(is_double('1111111'), '1212121')
+    def sum_equal_70(self):
+        self.assertEqua(sum_equal_70('102040'), 70)
+    def credit_check(self):
+        self.assertEqual(credit_check('5541808923795240'), 'The number is valid!')
+    def credit_check(self):
+        self.assertEqual(credit_check("4024007136512380"), 'The number is valid!')
+    def credit_check(self):
+        self.assertEqual(credit_check("6011797668867828"), 'The number is valid!')
+    def credit_check(self):
+        self.assertEqual(credit_check("5541801923795240"), "The number is invalid!")
+    def credit_check(self):
+        self.assertEqual(credit_check("4024007106512380"), "The number is invalid!")
+    def credit_check(self):
+        self.assertEqual(credit_check("6011797668868728"), "The number is invalid!")
+    
 
-print(credit_check("5541801923795240") == "The number is invalid!")
-print(credit_check("4024007106512380") == "The number is invalid!")
-print(credit_check("6011797668868728") == "The number is invalid!")
+
+if __name__ == '__main__':
+    unittest.main()
+
+# print(credit_check('5541808923795240') == "The number is valid!")
+# print(credit_check("4024007136512380") == "The number is valid!")
+# print(credit_check("6011797668867828") == "The number is valid!")
+
+# print(credit_check("5541801923795240") == "The number is invalid!")
+# print(credit_check("4024007106512380") == "The number is invalid!")
+# print(credit_check("6011797668868728") == "The number is invalid!")
